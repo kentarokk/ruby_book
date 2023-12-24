d6d5762db0edeb335147455d87e8a08c2bee3797
Kentaro Kato <ken.k.jul2@gmail.com>
2023-12-24 14:58:36 +0900

    -ch4-

---
+++ lib/rgb.rb
@@ -0,0 +1,35 @@
+def to_hex(r,g,b)
+    # '#' + r.to_s(16).rjust(2,'0') + g.to_s(16).rjust(2,'0') + b.to_s(16).rjust(2,'0')
+
+    # hex = '#'
+    # [r,g,b].each do |n|
+    #     hex += n.to_s(16).rjust(2,'0')
+    # end
+    # hex
+
+    [r,g,b].sum('#') do |n|
+        n.to_s(16).rjust(2,'0')
+    end
+end
+
+def to_ints(hex)
+    # r = hex[1..2]
+    # g = hex[3..4]
+    # b = hex[5..6]
+
+    # r,g,b = hex.scan(/\w\w/).map do |s|
+    #   s.hex
+    # end
+    
+    hex.scan(/\w\w/).map(&:hex)
+
+    # ints = []
+    # [r,g,b].each do |s|
+    #     ints << s.hex
+    # end
+    # ints
+
+    # [r,g,b].map do |s|
+    #   s.hex
+    # end
+end
